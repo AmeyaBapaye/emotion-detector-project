ece1e0b882901ee4e3c722443e0d8f37f5df04f2
Ameya Bapaye <ameyabapaye@gmail.com>
2025-08-15 16:36:36 -0400

    Finish up task 3 objectives

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,6 +7,27 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=myobj, headers=header)
-
     formatted_response = json.loads(response.text)
-    print (formatted_response)
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    # Individual Scores
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+    
+    # Finding dominant emotion
+    dominant_emotion = "anger"
+    for key,value in emotions.items():
+        if value > emotions[dominant_emotion]:
+            dominant_emotion = key
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
