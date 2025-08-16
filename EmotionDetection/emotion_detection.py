7a23ddfd32ea46906bc6eb64f1bd452062c9f66a
Ameya Bapaye <ameyabapaye@gmail.com>
2025-08-16 16:37:41 -0400

    Complete task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,22 +7,32 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response["emotionPredictions"][0]["emotion"]
 
-    # Individual Scores
-    anger_score = emotions["anger"]
-    disgust_score = emotions["disgust"]
-    fear_score = emotions["fear"]
-    joy_score = emotions["joy"]
-    sadness_score = emotions["sadness"]
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+        # Individual Scores
+        anger_score = emotions["anger"]
+        disgust_score = emotions["disgust"]
+        fear_score = emotions["fear"]
+        joy_score = emotions["joy"]
+        sadness_score = emotions["sadness"]
+        
+        # Finding dominant emotion
+        dominant_emotion = "anger"
+        for key,value in emotions.items():
+            if value > emotions[dominant_emotion]:
+                dominant_emotion = key
     
-    # Finding dominant emotion
-    dominant_emotion = "anger"
-    for key,value in emotions.items():
-        if value > emotions[dominant_emotion]:
-            dominant_emotion = key
-
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
+    
     return {
         'anger': anger_score,
         'disgust': disgust_score,
